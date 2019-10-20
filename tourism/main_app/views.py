a0f53cf23546429f46cc3e13d10cdfb84b2087bf
Aneesh Bhat <imaneeshbhat@gmail.com>
2019-10-21 00:20:32 +0530

    working search_page, index

--- tourism/main_app/views.py
+++ tourism/main_app/views.py
@@ -8,11 +8,34 @@ from django.views import generic
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.renderers import TemplateHTMLRenderer
 
+from .models import User, Hotel
+
+
+
+# Create your views here.
 class Index(TemplateView):
     template_name = 'main_app/index.html'
 
 class SearchPage(TemplateView):
 	template_name = 'main_app/search_page.html'
+
+	def post(self, request):
+		data = request.POST.get("search_bar", "")
+
+		list_of_hotels = list(Hotel.objects.all())
+
+		return render(request, self.template_name, {'list_of_hotels':list_of_hotels})
+	
+
+
+
+
+
+class SearchPageAPI(APIView):
+	def get(self, request):
+		pass
+
 
