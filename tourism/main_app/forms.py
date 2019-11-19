b4239e221889ce7b24a6dcc71d80f007b4ee3d75
Aneesh Bhat <imaneeshbhat@gmail.com>
2019-11-19 19:16:59 +0530

    fix error with payment page form

--- tourism/main_app/forms.py
+++ tourism/main_app/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+import datetime
 
 class UserForm(forms.ModelForm):
 
@@ -8,3 +9,22 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password']
+
+
+class PaymentPage(forms.Form):
+	first_name = forms.CharField(label="First Name", max_length=50)
+	last_name = forms.CharField(label="Last Name", max_length=50)
+	age = forms.IntegerField(label="Age")
+	gender = forms.CharField(label="Gender", max_length=1)
+
+	address = forms.CharField(label="Address", max_length=100)
+	city = forms.CharField(label="City", max_length=100)
+	zipcode = forms.CharField(label="Zip Code", max_length=6)
+
+	check_in = forms.DateField(label="Check-in Date")
+	check_out = forms.DateField(label="Check-out Date")
+
+	hotel_name = forms.CharField(label="Hotel Name", max_length=100)
+
+	class Meta:
+		fields = ['first_name', 'last_name', 'age']
