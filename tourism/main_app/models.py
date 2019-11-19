52d98af8c91c8bd88c44ebd2dd5863a280fe8fc7
arjl153 <arjl153@gmail.com>
2019-11-19 12:40:04 +0530

    selection page error
    selection page error

--- tourism/main_app/models.py
+++ tourism/main_app/models.py
@@ -13,22 +13,22 @@ class Session(models.Model):
 		return (str(self.pk))
 
 class User(models.Model):
-	name = models.CharField(max_length = 50)
-	age = models.IntegerField(default = 0)
-	email = models.CharField(max_length = 50)
-	password = models.CharField(max_length = 50)
+	name = models.CharField(max_length=50)
+	age = models.IntegerField(default=0)
+	email = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name + " " +self.age
 
 class Hotel(models.Model):
-	name = models.CharField(max_length = 50)
-	city = models.CharField(max_length = 50)
-	country = models.CharField(max_length = 50)
-	address = models.CharField(max_length = 100)
-	startPrice = models.FloatField(default = 0.0)
-	endPrice = models.FloatField(default = 0.0)
-	image = models.ImageField(null=True, blank=True)
+	name = models.CharField(max_length=50)
+	city = models.CharField(max_length=50)
+	country = models.CharField(max_length=50)
+	address = models.CharField(max_length=100)
+	startPrice = models.FloatField(default=0.0)
+	endPrice = models.FloatField(default=0.0)
+	image_url = models.CharField(default="empty", max_length=100)
 
 	def __str__(self):
 		return (self.name + " " +self.city + " " + self.country + " " 
--- tourism/main_app/views.py
+++ tourism/main_app/views.py
@@ -13,9 +13,59 @@ from rest_framework.renderers import TemplateHTMLRenderer
 	
 from .models import User, Hotel, Session
 
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
+from .forms import UserForm
 
 # Create your views here.
+class UserFormView(TemplateView):
+	form_class = UserForm
+	template_name = 'main_app/register.html'
+	
+	#display blank form for new login
+	def get(self, request):
+		if "username" in request.session:
+			del request.session["username"]
+			logout(request)
+
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form':form})
+
+	#after submit
+	def post(self, request):
+		form = UserForm(data=request.POST)
+
+		#print(type(form),form, "123\n\n\n\n")
+		un = request.POST.get("username", " ")
+		print(request.POST)
+
+		if(form.is_valid()):
+			user = form.save(commit=False)#not saved to db yet
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			print("\n\n\n\n",username, password)
+			user.set_password(password) #set password 
+
+			user.save()
+
+			#return user if correct
+
+			user = authenticate(username=username, password=password)
+			
+			if user is not None:
+				
+				if user.is_active:
+					login(request, user)
+					request.session['username'] = username
+					#refer user as request.user
+					return render(request, 'main_app/index.html')
+
+		return render(request, self.template_name, {'form':form})
+
+
+
 class Index(TemplateView):
     template_name = 'main_app/index.html'
 
@@ -41,13 +91,11 @@ class GetCities(APIView):
 class SelectionPage(TemplateView):
 	template_name = 'main_app/selection_page.html'
 
-	def get(self, request):
-		session_object = Session.objects.get(pk = 1)
-		check_in = session_object.check_in
-		check_out = session_object.check_out
-		print("\n\n\n\n\n\n\n\n\n", check_in, check_out)
-		return render(request, self.template_name, {'check_in': check_in,
-		 'check_out': check_out})
+	def post(self, request):
+		hotel_name = request.POST.get("hotel-name", " ")
+		hotel = Hotel.objects.get(name = hotel_name)
+
+		return render(request, self.template_name, {'hotel_name': result})
 
 class SearchPage(TemplateView):
 	template_name = 'main_app/search_page.html'
@@ -81,7 +129,21 @@ class SearchPage(TemplateView):
 
 class LoginPage(TemplateView):
 	template_name = 'main_app/login_page.html'
-	
+
+	def post(self, request):
+		username = request.POST.get("username", " ")
+		password = request.POST.get("password", " ")
+		user = authenticate(username=username, password=password)
+			
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				request.session['username'] = username
+				return render(request, 'main_app/index.html')
+
+
+
+		return render(request, self.template_name)
 
 
 
